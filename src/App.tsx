295606da9151432a8417dc641567e4b8e2a29f1a
Lucas <lucassantoslac3@gmail.com>
2024-06-22 17:19:31 -0300

    teste com erros

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import Todo from './components/todo.tsx';
 import TodoForm from './components/TodoForm.tsx';
 
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -42,11 +43,26 @@ function App() {
 
   }
 
+  const removeTodo = (id: number) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo => 
+      todo.id !== id ? todo : null))
+
+    setTodos(filteredTodos)
+  }
+
+  const completeTodo = (id: number) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+
   return <div className='app'>
     <h1>Lista de Tarefas</h1>
+    
     <div className="todo-list">
       {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo}/>
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))}
     </div>
     <TodoForm addTodo={addTodo}/>
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -5,19 +5,33 @@ export type TodoProps = {
       category: string;
       isCompleted: boolean;
     }
+
+    removeTodo: {
+      id: number;
+      text: string;
+      category: string;
+      isCompleted: boolean;
+    }
+
+    completeTodo: {
+      id: number;
+      text: string;
+      category: string;
+      isCompleted: boolean;
+    }
 }
 
 
-const Todo = ({todo}: TodoProps) => {
+const Todo = ({todo, removeTodo, completeTodo}: TodoProps) => {
   return(
-    <div className="todo">
+    <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
           <div className="content">
             <p>{todo.text}</p>
             <p className="category">({todo.category})</p>
           </div>
           <div>
-            <button className="complete">Completar</button>
-            <button className="remove">x</button>
+            <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
+            <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
           </div>
         </div>
   )
